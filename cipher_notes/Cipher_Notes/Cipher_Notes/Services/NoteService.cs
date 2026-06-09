8a0b7bca0c2c1dd58c578abb4bf979152e106235
Supreme_Engineer98 <gmavropouloscoding@gmail.com>
2026-06-09 22:10:30 +0300

    Created the SecureNote service and CreateNote method

---
+++ cipher_notes/Cipher_Notes/Cipher_Notes/Services/NoteService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cipher_Notes.Services
+{
+    public class NoteService
+    {
+        //declaring variables
+        private DatabaseService databaseService;
+
+        private EncryptionService encryptionService;
+
+        //declare constructor
+        public NoteService()
+        {
+
+        }
+
+        //create note method
+        public void CreateNote(string title, string content, string password)
+        {
+            //try-catch method to handle unexpected errors
+            try
+            {
+                
+            }catch(Exception ex)
+            {
+                throw new Exception("Cannot create the note", ex);
+            }
+        }
+    }
+}
