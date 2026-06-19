a4be659eb25a004105ae8477cdcc8f5901a4597b
Supreme_Engineer98 <gmavropouloscoding@gmail.com>
2026-06-19 20:26:20 +0300

    Created an Interface for EncryptionService, updated NoteService and MauiProgram.cs in order to make it applciable

---
+++ cipher_notes/Cipher_Notes/Cipher_Notes.Core/Interfaces/IEncryptionService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cipher_Notes.Core.Interfaces
+{
+    //INTERFACE class for encryption service. 
+    public interface IEncryptionService
+    {
+        //interface for encryptnote method
+        (string CipherText, string Salt, string IV) EncryptNote(string content, string password);
+
+        //interface for decryptcontent method
+        string DecryptContent(string encrypted_content, string password, string salt, string iv);
+
+       
+
+    }
+}
