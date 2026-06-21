02a4e598e35f208ed91378089f22257609353169
Supreme_Engineer98 <gmavropouloscoding@gmail.com>
2026-06-21 16:25:01 +0300

    Initialized the constructor in TestNoteService test class. Now I will create a PR and fix another important part which is to create a Database Exception class

--- cipher_notes/Cipher_Notes/Cipher_Notes.Tests/TestNoteService.cs
+++ cipher_notes/Cipher_Notes/Cipher_Notes.Tests/TestNoteService.cs
@@ -13,5 +13,26 @@ namespace Cipher_Notes.Tests
     //unit tests for NotService
     public class TestNoteService
     {
-    }
+
+        //declare variables
+        private readonly NoteService _noteService;//create NoteService object
+        private readonly Mock<IDatabaseService> mocked_db;// create a mocked DatabaseService object
+        private readonly Mock<IEncryptionService> mocked_encryption;//crreate a mocked encryption's service object
+
+
+        //create a constructor and create a new object each time the test runs to nullify previous data for each test
+        public TestNoteService()
+        {
+            mocked_db = new Mock<IDatabaseService>(); // initialize object mocked_db
+            mocked_encryption = new Mock<IEncryptionService>(); // initialize object mocked_encryption
+
+            _noteService = new NoteService( //initialize note's service object and attach Objects of interface's instances
+                mocked_db.Object,
+                mocked_encryption.Object
+          );
+
+        }
+
+
+        }
 }
