9ae865c48416e6fb5098f4479d4a8a8c3c45e44d
Supreme_Engineer98 <gmavropouloscoding@gmail.com>
2026-06-21 14:50:54 +0300

    Installed Moq Nuget package

---
+++ cipher_notes/Cipher_Notes/Cipher_Notes.Tests/TestNoteService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Cipher_Notes.Core.Models;
+using Moq;
+using Cipher_Notes.Core.Services;
+
+using Cipher_Notes.Core.Exceptions;
+using Cipher_Notes.Core.Interfaces;
+
+namespace Cipher_Notes.Tests
+{
+    //unit tests for NotService
+    public class TestNoteService
+    {
+    }
+}
