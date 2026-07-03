5f9ada49289cb61c5c756de3cee69bc8c347ca60
Supreme_Engineer98 <gmavropouloscoding@gmail.com>
2026-07-03 12:39:21 +0300

    Moved ViewModels into CipherNotes.Core in order to be able to test my methods.

--- cipher_notes/Cipher_Notes/Cipher_Notes.Core/ViewModels/ViewNoteViewModel.cs
+++ cipher_notes/Cipher_Notes/Cipher_Notes.Core/ViewModels/ViewNoteViewModel.cs
@@ -8,7 +8,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
 
-namespace Cipher_Notes.ViewModels
+namespace Cipher_Notes.Core.ViewModels
 {
     //ViewNote's view model for ViewNote page
     public partial class ViewNoteViewModel : ObservableObject
