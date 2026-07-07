c9021545a2c7d01ca33b4e63eafd417d4c107b1b
Supreme_Engineer98 <gmavropouloscoding@gmail.com>
2026-07-07 17:17:22 +0300

    Set up a class to test NoteListViewModel.

---
+++ cipher_notes/Cipher_Notes/Cipher_Notes.Tests/TestNoteListViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Cipher_Notes.Core.Exceptions;
+using Cipher_Notes.Core.Interfaces;
+using Cipher_Notes.Core.Models;
+using Cipher_Notes.Core.Services;
+using Cipher_Notes.Core.ViewModels;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Text;
+using Xunit.Sdk;
+
+
+namespace Cipher_Notes.Tests
+{
+    public class TestNoteListViewModel
+    {
+
+        //create a NoteListViewModel object and a NoteService's mocked object
+        private readonly Mock<INoteService> note_service_mocked;
+
+        private readonly NoteListViewModel note_list_view_model;
+
+        //declare constructor
+        public TestNoteListViewModel()
+        {
+            //initialize a new note_service_mocked object and a note_list_view_model object each time a test run
+            note_service_mocked = new Mock<INoteService>();
+            note_list_view_model = new NoteListViewModel(note_service_mocked.Object);
+        }
+    }
+}
