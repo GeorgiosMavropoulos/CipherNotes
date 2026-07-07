ff001d9c62e951044a6406766255c18eb7f6f8e6
Supreme_Engineer98 <gmavropouloscoding@gmail.com>
2026-07-07 16:26:35 +0300

    I created a test to test that DecryptNote from DecryptNoteViewModel works as expected.To make this test work, I altered DecryptNoteViewModel and created a new ObservableProperty called Id.Test passed

--- cipher_notes/Cipher_Notes/Cipher_Notes.Tests/TestDecryptNoteViewModel.cs
+++ cipher_notes/Cipher_Notes/Cipher_Notes.Tests/TestDecryptNoteViewModel.cs
@@ -33,7 +33,42 @@ namespace Cipher_Notes.Tests
         }
 
 
-        //test
+        //test DecryptNote successfully decrypts note
+        [Fact]
+        public async Task Test_DecryptNote_Successfully_Decrypt_Notes()
+        {
+            //Arrange
+            
+            //declare variables
+            var content = "test";
+            var title = "test";
+            var password = "password";
+            var decrypted_text = "decrypted test";
+
+            //assign variable's values into model's properties
+            decrypt_viewModel.Password = password;
+            decrypt_viewModel.Id = 1;
+
+           
+
+            //set up _note_service_mocked to return the created note when DecryptNote is being called
+            _note_service_mocked.Setup(x => x.DecryptNote(1, password)).Returns(Task.FromResult(decrypted_text));
+
+            //Act
+            //call DecryptNoteCommand
+            await decrypt_viewModel.DecryptNoteCommand.ExecuteAsync(1);
+
+            //Assert
+
+            //verify DecryptNote from NoteService has been called once
+            _note_service_mocked.Verify(x => x.DecryptNote(1, password), Times.Once);
+
+
+            //verify decrypted_text is equals with decrypt_viewmodel's Decrypted_content
+            Assert.Equal(decrypted_text, decrypt_viewModel.Decrypted_content);
+
+
+        }
 
     }
 }
