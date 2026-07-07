a72e0736abf2304a582c52725f79e7e7bcb26432
Supreme_Engineer98 <gmavropouloscoding@gmail.com>
2026-07-07 17:39:24 +0300

    Created a test method to test that LoadNotes successfully returns the existed notes.Test passed.

--- cipher_notes/Cipher_Notes/Cipher_Notes.Tests/TestNoteListViewModel.cs
+++ cipher_notes/Cipher_Notes/Cipher_Notes.Tests/TestNoteListViewModel.cs
@@ -28,5 +28,56 @@ namespace Cipher_Notes.Tests
             note_service_mocked = new Mock<INoteService>();
             note_list_view_model = new NoteListViewModel(note_service_mocked.Object);
         }
+
+
+        //test load notes successfully returns existing notes
+        [Fact]
+        public async Task Test_LoadNotes_Successfully_Loads_Existing_Notes()
+        {
+            //Arrange
+            //create 2 notes
+            var note1 = new SecureNotes
+            {
+                Id = 1,
+                Title = "Note 1",
+                Encrypted_content = "content1",
+                Salt = "salt1",
+                IV = "iv1"
+            };
+
+            var note2 = new SecureNotes
+            {
+                Id = 2,
+                Title = "Note 2",
+                Encrypted_content = "content2",
+                Salt = "salt2",
+                IV = "iv2"
+            };
+
+            var note_list = new List<SecureNotes> { note1, note2 };
+
+            //set up GetAllNotes from NoteService to return the list from above when GetAllNotes is being called
+            note_service_mocked.Setup(x => x.GetAllNotes()).ReturnsAsync(note_list);
+
+            //Act
+            //call LoadNotes from ViewModel using LoadNotesCommand
+            await note_list_view_model.LoadNotesCommand.ExecuteAsync(null);
+
+            //Assert
+            
+            //verify GetAllNotes has been called once
+            note_service_mocked.Verify(x => x.GetAllNotes(), Times.Once());
+
+            //verify that 2 notes have been returned
+            Assert.Equal(2, note_list.Count);
+
+            //verify that note1's title is equals to the returned note's title
+            Assert.Equal(note1.Title, note_list[0].Title);
+
+            //verify that note2's title is equals to the returned note's title
+            Assert.Equal(note2.Title, note_list[1].Title);
+
+            
+        }
     }
 }
